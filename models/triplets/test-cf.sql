e0c2c3435b31615a098307d779b4ce1cae408d24
irischen <irischen@kkcompany.com>
2023-07-26 21:39:21 +0800

    fix: Fix the test case error caused by incorrect column name

--- models/triplets/test-cf.sql
+++ models/triplets/test-cf.sql
@@ -2,4 +2,4 @@ select distinct
     user_id,
     item_id
 from {{ref('intersect')}}
-where start_time >= '{{ var("target_date") }}' and user_id in (select user_id from {{ref('train-cf')}})
+where start_time >= '{{ var("target_date") }}' and user_id in (select column0 from {{ref('train-cf')}})
